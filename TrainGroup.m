4424f9404061ffe49ea60d35d4f64d1f5600108c
Gujojo <gujh17@mails.tsinghua.edu.cn>
2019-12-02 14:16:35 +0800

    添加了理想带通滤波，gcc全面优于二次相关，目前最低误差在2-3左右，但PHAT始终是错的

--- TrainGroup.m
+++ TrainGroup.m
@@ -9,6 +9,7 @@ trainNum = length(angleAns);
 
 %% 音频处理
 angle = zeros(trainNum, 1, 'double');
+filterFlag = 0;
 
 global wave;
 k = 10;
@@ -16,11 +17,13 @@ for index = 1: trainNum
     [wave, Fs] = audioread(['train\', num2str(index), '.wav']);
     wave = resample(wave, k, 1);
     Fs = Fs*k;
-%     figure;
-%     subplot(2, 1, 1);
-%     plot(wave(: , 1));
-%     subplot(2, 1, 2);
-%     plot(wave(: , 2));
+    
+%     if filterFlag
+%         Filter = ones(45, 1);
+%         tmp1 = conv(wave(: , 1), Filter);
+%         tmp2 = conv(wave(: , 2), Filter);
+%         wave = [tmp1, tmp2];
+%     end
     angle(index) = AngleEstimate(Fs);
 end
 
@@ -41,12 +44,24 @@ function angle = AngleEstimate(Fs)
     psdF = fft(wave);
     psdF1 = psdF(: , 1);
     psdF2 = psdF(: , 2);
-    corrF = psdF1 .* conj(psdF2);
-    SCOT = 1 ./ (psdF1 .* psdF2).^0.5;
-    PHAT = 1 ./ abs(corrF);
     
+    %% 对功率谱FFT结果进行滤波
+    FilterF = ones(length(psdF1), 1, 'double')*0.01;
+    indexHigh = 2500;
+    indexLow = 200;
+    FilterF(indexLow: indexHigh) = 1;
+    FilterF(end-indexHigh: end-indexLow) = 1;
+    psdF1 = psdF1 .* FilterF;
+    psdF2 = psdF2 .* FilterF;
+    
+    %% 计算相关函数
+    corrF = psdF1 .* conj(psdF2);
+    SCOT = 1 ./ abs((psdF1 .* psdF2).^0.5);
+    PHAT = 1 ./ abs(corrF);
+    HB = abs(corrF) ./ abs(psdF1 .* psdF2);
 %     corrT = ifft(corrF .* PHAT);
 %     corrT = ifft(corrF .* SCOT);
+%     corrT = ifft(corrF .* HB);
     corrT = ifft(corrF);
 %     plot(corrT)
     
@@ -80,12 +95,22 @@ function angle = SecondaryCorrelation(Fs)
     psdF = fft(wave);
     psdF1 = psdF(: , 1);
     psdF2 = psdF(: , 2);
+%% 对功率谱FFT结果进行滤波
+    FilterF = ones(length(psdF1), 1, 'double')*0.01;;
+    indexHigh = 2500;
+    indexLow = 200;
+    FilterF(indexLow: indexHigh) = 1;
+    FilterF(end-indexHigh: end-indexLow) = 1;
+    psdF1 = psdF1 .* FilterF;
+    psdF2 = psdF2 .* FilterF;
+    
+%% 计算相关函数
     corrF11 = psdF1 .* conj(psdF1);
     corrF12 = psdF1 .* conj(psdF2);
     corrF22 = psdF2 .* conj(psdF2);
     
     corrF = corrF11 .* conj(corrF12);
-    SCOT = 1 ./ (corrF11 .* corrF22).^0.5;
+    SCOT = 1 ./ abs((corrF11 .* corrF22).^0.5);
     PHAT = 1 ./ abs(corrF);
     
 %     corrT = ifft(corrF);
